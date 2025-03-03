5acf407bd54a7d1a6ee094b3e3a5d3b8f47b91cd
Blair Crumbly <blaircrumbly@gmail.com>
2025-03-03 16:20:21 -0500

    all tests passed

--- lib/song.py
+++ lib/song.py
@@ -1,56 +1,45 @@
-
-
 class Song:
-    #todo
-    #init name artist and genre
-    #create isntance
-    #add count in top of class at 0, then the incrementor where the song is created
-    #add classmethod to access class variable
-    #* Class "Song" in song.py keeps count of Songs for each genre. - KeyError: 'Rap'
 
     count = 0
-    genres = {}
+    genres = []
     artists = []
-    songsperartist = {}
-    
+    genre_count = {}
+    artist_count = {}
 
-    def __init__(self, name, artist, genre):
+    def __init__(self, name, artist, genre) -> None:
         self.name = name
         self.artist = artist
         self.genre = genre
-        Song.count += 1
-        Song.artists.append(self.artist)
-        
-        if genre in Song.genres:
-            Song.genres[genre] += 1
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+        self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if cls.genre_count.get(genre):
+            cls.genre_count[genre] += 1
         else:
-            Song.genres[genre] = 1
-        
-        if artist in Song.songsperartist:
-            Song.songsperartist[artist] += 1
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if cls.artist_count.get(artist):
+            cls.artist_count[artist] += 1
         else:
-            Song.songsperartist[artist] = 1
-
-    @classmethod
-    def get_count(cls):
-        return cls.count
-    @classmethod
-    def get_genre_count(cls):
-        return cls.genres
-    @classmethod
-    def get_artists(cls):
-        return cls.artists
-    @classmethod
-    def get_song_per_artist(cls):
-        return cls.songsperartist
-
-TestSong = Song("99 Problems", "Jay Z", "Rap")
-TestSong2 = Song("No Role Modelz", "J. Cole", "Rap")
-TestSong3 = Song("Snooze", "SZA", "R&B")
-TestSong4 = Song("No more hiding", "SZA", "R&B")
-TestSong5 = Song("Banned in D.C.", "Bad Brains", "Punk")
-
-print(Song.get_count())
-print(Song.get_genre_count())
-print(Song.get_artists())
-print(Song.get_song_per_artist())
+            cls.artist_count[artist] = 1
